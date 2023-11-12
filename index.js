a5a49177e783bc1a53ecada1ff6c3a04b3658b75
Max Lima <88208329+MaxEvk@users.noreply.github.com>
2023-11-12 11:03:59 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+function onChangeEmail() {
+  toggleButtonsDisable();
+  toggleEmailErros();
+}
+function onChangePassword() {
+  toggleButtonsDisable();
+  togglePasswordErros();
+}
+function isEmailValid() {
+  const email = document.getElementById("email").value;
+  if (!email) {
+    return false;
+  }
+  return validateEmail(email);
+}
+
+function toggleEmailErros() {
+  const email = document.getElementById("email").value;
+  if (!email) {
+    document.getElementById("email-required-error").style.display = "block";
+  } else {
+    document.getElementById("email-required-error").style.display = "none";
+  }
+
+  if (validateEmail(email)) {
+    document.getElementById("email-invalid-error").style.display = "none";
+  } else {
+    document.getElementById("email-invalid-error").style.display = "block";
+  }
+}
+
+function togglePasswordErros() {
+  const password = document.getElementById("password").value;
+  if (!password) {
+    document.getElementById("password-requerid-error").style.display = "block";
+  } else {
+    document.getElementById("password-requerid-error").style.display = "none";
+  }
+}
+
+function toggleButtonsDisable() {
+  const emailValid = isEmailValid();
+  document.getElementById("recover-password-button").disabled = !emailValid;
+
+  const passwordValid = isPasswordValid();
+  document.getElementById("login-button").disabled =
+    !emailValid || !passwordValid;
+}
+
+function isPasswordValid() {
+  const password = document.getElementById("password").value;
+  if (!password) {
+    return false;
+  }
+  return true;
+}
+
+function validateEmail(email) {
+  return /\S+@\S+\.\S+/.test(email);
+}
